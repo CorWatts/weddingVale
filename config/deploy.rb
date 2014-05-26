ae44f5336c9ee2a28c749f135c5d365a06c2cd3b
Corey Watts <cwatts@brightidea.com>
2014-05-25 18:07:45 -0700

    Got cap task working properly. Also upgraded cap.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for Capistrano 3.2.1
+lock '3.2.1'
 
 set :application, 'WeddingVale'
 set :repo_url, 'ssh://git@errethak.be:9000/samWedding'
